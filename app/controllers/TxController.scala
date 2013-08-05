9493d95239f7752ddac2a151027a9d60138b5446
Karel Maesen <karel@geovise.com>
2013-08-05 23:58:24 +0200

    Adds the tx/update API POST method.

--- app/controllers/TxController.scala
+++ app/controllers/TxController.scala
@@ -6,6 +6,8 @@ import repositories.{MongoRepository, MongoBufferedWriter}
 import com.mongodb.casbah.WriteConcern
 import com.mongodb.util.{JSONParseException, JSON => Parser}
 import com.mongodb.casbah.Imports._
+import com.mongodb.WriteResult
+import scala.Some
 
 /**
  * @author Karel Maesen, Geovise BVBA
@@ -13,10 +15,11 @@ import com.mongodb.casbah.Imports._
  */
 object TxController extends Controller {
 
+  type UpdateOp = (MongoCollection, Seq[MongoDBObject]) => WriteResult
 
 
   def insert(db: String, col: String) = {
-    val writer = try {
+    val jsonWritingBodyParser = try {
       val writer = new MongoBufferedWriter(db, col)
       Some(ReactiveGeoJson.bodyParser(writer))
     } catch {
@@ -24,25 +27,33 @@ object TxController extends Controller {
     }
     val parser = parse.using {
       rh =>
-        writer.getOrElse(parse.error(NotFound(s"$db/$col does not exist or is not a spatial collection")))
+        jsonWritingBodyParser.getOrElse(parse.error(NotFound(s"$db/$col does not exist or is not a spatial collection")))
     }
     Action(parser) {
       request => Ok(request.body.msg)
     }
   }
 
-  def remove(db: String, col: String) = Action(BodyParsers.parse.tolerantText) {
-    request => {
+  def remove(db: String, col: String) = mkUpdateAction(db, col)(
+    extractor = extractKey(_, "query"),
+    updateOp = (coll, doc) => coll.remove(doc(0), WriteConcern.Safe))
+
+  def update(db: String, col: String) = mkUpdateAction(db,col)(
+    extractor = extractKey(_,"query", "update"),
+    updateOp = (coll, doc) => coll.update(doc(0), doc(1), concern=WriteConcern.Safe) )
+
+
+  def mkUpdateAction( db: String, col: String) (extractor: MongoDBObject => Seq[MongoDBObject],
+                                                updateOp : UpdateOp ) =
+    Action(BodyParsers.parse.tolerantText) {
+    implicit request => {
       val (collectionOpt, _) = MongoRepository.getCollection(db, col)
-      val reqBody = toDBObject(request.body) match {
-        case Right(body) => extractKey(body, "query")
-        case Left(err) => Left(err)
-      }
+      val reqBody = toDBObject(request.body) (extractor)
       (collectionOpt, reqBody) match {
         case (Some(collection), Right(doc)) =>
-          val result = collection.remove(doc, WriteConcern.Safe)
-          if (result.getLastError.ok) Ok(s"Deleted ${result.getN}")
-          else InternalServerError(s"Delete error: ${result.getLastError.getErrorMessage}")
+          val result = updateOp(collection, doc)
+          if (result.getLastError.ok) Ok(s"Records affected ${result.getN}")
+          else InternalServerError(s"Database error: ${result.getLastError.getErrorMessage}")
 
         case (None, _) => NotFound(s"$db/$col does not exist.")
         case (_, Left(err)) => BadRequest(err)
@@ -50,24 +61,25 @@ object TxController extends Controller {
     }
   }
 
-  def toDBObject(txt: String): Either[String, MongoDBObject] = {
+  def toDBObject(txt: String)(implicit extractor: MongoDBObject => Seq[MongoDBObject]): Either[String, Seq[MongoDBObject]] = {
     try {
       Parser.parse(txt) match {
-        case r: DBObject => Right(r)
+        case r: DBObject => Right(extractor(r))
         case _ => Left(s"'$txt' does not represent an object")
       }
     } catch {
       case e: JSONParseException => Left(s"'$txt' is not valid JSON")
+      case e: RuntimeException => Left(e.getMessage)
     }
   }
 
-  def extractKey(mobj: MongoDBObject, key: String): Either[String, MongoDBObject] = {
-    mobj.get(key) match {
-      case Some(value: DBObject) => Right(value)
-      case Some(_) => Left(s"Query property is not an object")
-      case None => Left(s"No query property in object")
+  def extractKey(mobj: MongoDBObject, keys: String*): Seq[MongoDBObject] =
+    for( key <- keys) yield mobj.get(key) match {
+      case Some(value: DBObject) => new MongoDBObject(value)
+      case Some(_) => throw new RuntimeException(s"$key property is not an object")
+      case None => throw new RuntimeException(s"No $key property in object")
     }
-  }
+
 
 
 }
