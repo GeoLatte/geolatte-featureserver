43167e5ea6d4684122dce4e2ecbc718e6277e8c1
Karel Maesen <karel@geovise.com>
2013-08-27 12:26:59 +0200

    Parametrizes script to transfer collections to postgis.

--- scripts/fs2psql.sh
+++ scripts/fs2psql.sh
@@ -1,1 +1,8 @@
-ogr2ogr -nln nstest -f "PostgreSQL" PG:dbname=nstest "http://localhost:9000/api/databases/test/nstest/download" OGRGeoJSON 
+#! /bin/bash
+if [[ -z "$1" || -z "$2" ]]
+then
+	echo "Usage: $0 <dbname> <collectionname>"
+else
+	ogr2ogr -nln $2 -f "PostgreSQL" PG:dbname=$1 "http://localhost:9000/api/databases/$1/$2/download" OGRGeoJSON 	
+fi
+
