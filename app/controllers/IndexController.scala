c8765b94261b12f1138580c46c9bf2c0161a82e6
Karel Maesen <karel@geovise.com>
2015-02-19 11:23:13 +0100

    Defines routes for index management.

---
+++ app/controllers/IndexController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import config.AppExecutionContexts
+
+object IndexController extends AbstractNoSqlController{
+
+  import AppExecutionContexts._
+
+  def post(db: String, collection: String) = play.mvc.Results.TODO
+
+  def list(db: String, collection: String) = play.mvc.Results.TODO
+
+  def get(db: String, collection: String, index: String) = play.mvc.Results.TODO
+
+  def delete(db: String, collection: String, index: String) = play.mvc.Results.TODO
+}
