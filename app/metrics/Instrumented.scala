e0f74503931aa95767565d31cf7dc66efdad8d71
Karel Maesen <karel@geovise.com>
2017-05-31 18:34:21 +0200

    Define new metrics in Prometheus Collector

---
+++ app/metrics/Instrumented.scala
@@ -0,0 +1,8 @@
+package metrics
+
+/**
+  * Created by Karel Maesen, Geovise BVBA on 31/05/17.
+  */
+trait Instrumented {
+
+}
--- app/metrics/PrometheusMetrics.scala
+++ app/metrics/PrometheusMetrics.scala
@@ -10,10 +10,30 @@ import play.api.Logger
 class PrometheusMetrics {
 
   val totalRequests = Counter.build()
-    .name("requests_total").help("Total requests.").register()
+    .name("requests_total").help("Number of HTTP requests.").register()
+
+  val repoOperations = Counter.build()
+    .name("operations").help("Number of featureserver operations")
+    .labelNames("operation", "database", "collection")
+    .register()
+
+  val queryOperations = Counter.build()
+    .name("spatial_queries").help("Number of spatial queries")
+    .labelNames("database", "collection")
+    .register()
+
+  val bboxHistogram = Histogram.build()
+    .name("spatial_queries_bbox").help("Diagonal in meters of bbox in spatial query")
+    .labelNames("database", "collection")
+    .register()
+
+  val returnedItemsHistogram = Histogram.build()
+    .name("spatial_queries_returned_items").help("Number of returned items by spatial query")
+    .labelNames("database", "collection")
+    .register()
 
   val failedRequests = Counter.build()
-    .name("requests_failed_total").help("Total failed requests.").register()
+    .name("requests_failed_total").help("Number of failed requests.").register()
 
   val requestLatency = Histogram.build()
     .name("requests_latency_seconds").help("Request latency in seconds.").register();
@@ -24,6 +44,10 @@ class PrometheusMetrics {
     CollectorRegistry.defaultRegistry.register(totalRequests)
     CollectorRegistry.defaultRegistry.register(failedRequests)
     CollectorRegistry.defaultRegistry.register(requestLatency)
+    CollectorRegistry.defaultRegistry.register(repoOperations)
+    CollectorRegistry.defaultRegistry.register(queryOperations)
+    CollectorRegistry.defaultRegistry.register(bboxHistogram)
+    CollectorRegistry.defaultRegistry.register(returnedItemsHistogram)
 
     // Register the default Hotspot collectors
     DefaultExports.initialize()
