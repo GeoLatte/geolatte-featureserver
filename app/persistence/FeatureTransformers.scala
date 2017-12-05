dfeb9d064e644aa8310ec7324e3bf9c0b971d3ef
Natal Vande Casteele <natal@libius.be>
2017-12-05 13:59:16 +0100

    Support for geometrycollections (incl empty) and other non basic geometries

--- app/persistence/FeatureTransformers.scala
+++ app/persistence/FeatureTransformers.scala
@@ -1,31 +1,108 @@
 package persistence
 
+import org.geolatte.geom
 import org.geolatte.geom._
-import org.geolatte.geom.curve.{ MortonCode, MortonContext }
-import play.api.data.validation.ValidationError
+import org.geolatte.geom.crs.CrsId
 import play.api.libs.functional.syntax._
-import play.api.libs.json.Reads._
-import play.api.libs.json._
+import play.api.libs.json.{ Json, _ }
+
 /**
- * @author Karel Maesen, Geovise BVBA
- *         creation-date: 10/18/13
- */
+  * @author Karel Maesen, Geovise BVBA
+  *         creation-date: 10/18/13
+  */
 object FeatureTransformers {
 
-  import utilities.GeometryReaders._
+  /**
+    * Helpermethod that creates a geolatte pointsequence starting from an array containing coordinate arrays
+    *
+    * @param coordinates an array containing coordinate arrays
+    * @return a geolatte pointsequence or null if the coordinatesequence was null
+    */
+  private def createPointSequence(coordinates: Array[Array[Double]])(implicit crsId: CrsId): PointSequence = {
+    if (coordinates.isEmpty) {
+      PointCollectionFactory.createEmpty
+    } else {
+      val df: DimensionalFlag = if (coordinates.head.length == 4) DimensionalFlag.d3DM
+      else if (coordinates.head.length == 3) DimensionalFlag.d3D else DimensionalFlag.d2D
+      val psb: PointSequenceBuilder = PointSequenceBuilders.variableSized(df, crsId)
+      coordinates.foreach(coordinate => psb.add(coordinate))
+      psb.toPointSequence
+    }
+  }
+
+  implicit def pointReads(implicit crsId: CrsId): Reads[Point] =
+    (__ \ "coordinates").read[Array[Double]]
+      .map(coordinates => {
+        if (coordinates.length == 2) {
+          Points.create2D(coordinates(0), coordinates(1), crsId)
+        } else if (coordinates.length == 3) {
+          Points.create3D(coordinates(0), coordinates(1), coordinates(2), crsId)
+        } else {
+          val z: Double = coordinates(2)
+          val m: Double = coordinates(3)
+          if (z.isNaN) {
+            Points.create2DM(coordinates(0), coordinates(1), m, crsId)
+          } else {
+            Points.create3DM(coordinates(0), coordinates(1), z, m, crsId)
+          }
+        }
+      })
+
+  implicit def lineStringReads(implicit crsId: CrsId): Reads[LineString] =
+    (__ \ "coordinates").read[Array[Array[Double]]]
+      .map(coordinates => new LineString(createPointSequence(coordinates)))
+
+  implicit def multiLineStringReads(implicit crsId: CrsId): Reads[MultiLineString] =
+    (__ \ "coordinates").read[Array[Array[Array[Double]]]]
+      .map(coordinates => new MultiLineString(coordinates.map(createPointSequence).map(new LineString(_))))
+
+  private def createPolygon(coordinates: Array[Array[Array[Double]]])(implicit crsId: CrsId): geom.Polygon =
+    new Polygon(coordinates.map(sequence => new LinearRing(createPointSequence(sequence))))
+
+  implicit def polygonReads(implicit crsId: CrsId): Reads[Polygon] =
+    (__ \ "coordinates").read[Array[Array[Array[Double]]]]
+      .map(coordinates => createPolygon(coordinates))
+
+  implicit def multiPolygonReads(implicit crsId: CrsId): Reads[MultiPolygon] =
+    (__ \ "coordinates").read[Array[Array[Array[Array[Double]]]]]
+      .map(coordinates => new MultiPolygon(coordinates.map(sequence => createPolygon(sequence))))
+
+  implicit def geometryCollectionReads(implicit crsId: CrsId): Reads[GeometryCollection] =
+    (__ \ "geometries").read[Array[JsValue]]
+      .map(geometries => new GeometryCollection(geometries.map(geometry => geometryReads.reads(geometry).get)))
+
+  implicit def geometryReads(implicit crsId: CrsId): Reads[Geometry] = (
+    (__ \ "type").read[String] and
+      __.json.pick
+    )((typeDiscriminator: String, js: JsValue) => {
+
+    typeDiscriminator match {
+      case "Point" => Json.fromJson[Point](js).get
+      case "LineString" => Json.fromJson[LineString](js).get
+      case "Polygon" => Json.fromJson[Polygon](js).get
+      case "MultiLineString" => Json.fromJson[MultiLineString](js).get
+      case "MultiPolygon" => Json.fromJson[MultiPolygon](js).get
+      case "GeometryCollection" => Json.fromJson[GeometryCollection](js).get
+    }
+  })
 
   /**
-   * Extracts the Envelope from the GeoJson
-   * @param extent the spatial extent
-   * @return
-   */
-  def envelopeTransformer(extent: Envelope): Reads[Polygon] =
-    (__ \ 'geometry \ 'coordinates).json.pick[JsArray] andThen
-      PositionReads.map { pos =>
-        pos.boundingBox
-      }.map { bbox =>
-        toPolygon(bbox.toEnvelope(extent.getCrsId))
+    * Extracts the Envelope from the GeoJson
+    *
+    * @return
+    */
+  def envelopeTransformer(extent: Envelope): Reads[Polygon] = {
+    implicit val crsId: CrsId = extent.getCrsId
+    (__ \ 'geometry).json.pick[JsObject] andThen
+      geometryReads.map { geometry =>
+        geometry.getEnvelope match {
+          case Envelope.EMPTY =>
+            Polygon.createEmpty()
+          case env: Envelope =>
+            toPolygon(env)
+        }
       }
+  }
 
   def toPolygon(envelope: Envelope): Polygon = {
     val builder = PointSequenceBuilders.fixedSized(5, DimensionalFlag.d2D, envelope.getCrsId)
