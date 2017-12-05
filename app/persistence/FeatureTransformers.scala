7207ab65d982a2f64cda6fd0598f524440998bec
Karel Maesen <karel@geovise.com>
2017-12-05 17:14:18 +0100

    Adds MultiPoint geometry support

--- app/persistence/FeatureTransformers.scala
+++ app/persistence/FeatureTransformers.scala
@@ -7,17 +7,17 @@ import play.api.libs.functional.syntax._
 import play.api.libs.json.{ Json, _ }
 
 /**
-  * @author Karel Maesen, Geovise BVBA
-  *         creation-date: 10/18/13
-  */
+ * @author Karel Maesen, Geovise BVBA
+ *         creation-date: 10/18/13
+ */
 object FeatureTransformers {
 
   /**
-    * Helpermethod that creates a geolatte pointsequence starting from an array containing coordinate arrays
-    *
-    * @param coordinates an array containing coordinate arrays
-    * @return a geolatte pointsequence or null if the coordinatesequence was null
-    */
+   * Helpermethod that creates a geolatte pointsequence starting from an array containing coordinate arrays
+   *
+   * @param coordinates an array containing coordinate arrays
+   * @return a geolatte pointsequence or null if the coordinatesequence was null
+   */
   private def createPointSequence(coordinates: Array[Array[Double]])(implicit crsId: CrsId): PointSequence = {
     if (coordinates.isEmpty) {
       PointCollectionFactory.createEmpty
@@ -32,21 +32,23 @@ object FeatureTransformers {
 
   implicit def pointReads(implicit crsId: CrsId): Reads[Point] =
     (__ \ "coordinates").read[Array[Double]]
-      .map(coordinates => {
-        if (coordinates.length == 2) {
-          Points.create2D(coordinates(0), coordinates(1), crsId)
-        } else if (coordinates.length == 3) {
-          Points.create3D(coordinates(0), coordinates(1), coordinates(2), crsId)
-        } else {
-          val z: Double = coordinates(2)
-          val m: Double = coordinates(3)
-          if (z.isNaN) {
-            Points.create2DM(coordinates(0), coordinates(1), m, crsId)
-          } else {
-            Points.create3DM(coordinates(0), coordinates(1), z, m, crsId)
-          }
-        }
-      })
+      .map(createPoint(crsId, _))
+
+  private def createPoint(crsId: CrsId, coordinates: Array[Double]) = {
+    if (coordinates.length == 2) {
+      Points.create2D(coordinates(0), coordinates(1), crsId)
+    } else if (coordinates.length == 3) {
+      Points.create3D(coordinates(0), coordinates(1), coordinates(2), crsId)
+    } else {
+      val z: Double = coordinates(2)
+      val m: Double = coordinates(3)
+      if (z.isNaN) {
+        Points.create2DM(coordinates(0), coordinates(1), m, crsId)
+      } else {
+        Points.create3DM(coordinates(0), coordinates(1), z, m, crsId)
+      }
+    }
+  }
 
   implicit def lineStringReads(implicit crsId: CrsId): Reads[LineString] =
     (__ \ "coordinates").read[Array[Array[Double]]]
@@ -63,6 +65,13 @@ object FeatureTransformers {
     (__ \ "coordinates").read[Array[Array[Array[Double]]]]
       .map(coordinates => createPolygon(coordinates))
 
+  private def createMultiPoint(coordinates: Array[Array[Double]])(implicit crsId: CrsId): geom.MultiPoint =
+    new MultiPoint(coordinates.map(co => createPoint(crsId, co)))
+
+  implicit def multiPointReads(implicit crsId: CrsId): Reads[MultiPoint] =
+    (__ \ "coordinates").read[Array[Array[Double]]]
+      .map(createMultiPoint)
+
   implicit def multiPolygonReads(implicit crsId: CrsId): Reads[MultiPolygon] =
     (__ \ "coordinates").read[Array[Array[Array[Array[Double]]]]]
       .map(coordinates => new MultiPolygon(coordinates.map(sequence => createPolygon(sequence))))
@@ -73,24 +82,25 @@ object FeatureTransformers {
 
   implicit def geometryReads(implicit crsId: CrsId): Reads[Geometry] = (
     (__ \ "type").read[String] and
-      __.json.pick
-    )((typeDiscriminator: String, js: JsValue) => {
+    __.json.pick
+  )((typeDiscriminator: String, js: JsValue) => {
 
-    typeDiscriminator match {
-      case "Point" => Json.fromJson[Point](js).get
-      case "LineString" => Json.fromJson[LineString](js).get
-      case "Polygon" => Json.fromJson[Polygon](js).get
-      case "MultiLineString" => Json.fromJson[MultiLineString](js).get
-      case "MultiPolygon" => Json.fromJson[MultiPolygon](js).get
-      case "GeometryCollection" => Json.fromJson[GeometryCollection](js).get
-    }
-  })
+      typeDiscriminator match {
+        case "Point" => Json.fromJson[Point](js).get
+        case "LineString" => Json.fromJson[LineString](js).get
+        case "Polygon" => Json.fromJson[Polygon](js).get
+        case "MultiPoint" => Json.fromJson[MultiPoint](js).get
+        case "MultiLineString" => Json.fromJson[MultiLineString](js).get
+        case "MultiPolygon" => Json.fromJson[MultiPolygon](js).get
+        case "GeometryCollection" => Json.fromJson[GeometryCollection](js).get
+      }
+    })
 
   /**
-    * Extracts the Envelope from the GeoJson
-    *
-    * @return
-    */
+   * Extracts the Envelope from the GeoJson
+   *
+   * @return
+   */
   def envelopeTransformer(extent: Envelope): Reads[Polygon] = {
     implicit val crsId: CrsId = extent.getCrsId
     (__ \ 'geometry).json.pick[JsObject] andThen
