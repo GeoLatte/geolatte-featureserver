8a2cbf32eb4e29aa4ea1259a44d3b219c0dbd881
Karel Maesen <karel@geovise.com>
2018-05-18 16:24:31 +0200

    Complete QueryRender Specification tests

--- test/persistence/postgresql/PGQueryRenderSpec.scala
+++ test/persistence/postgresql/PGQueryRenderSpec.scala
@@ -94,6 +94,90 @@ class PGQueryRenderSpec extends Specification {
 
   }
 
+  "The PGRegularQueryRenderer " should {
+
+    val renderer = PGRegularQueryRenderer
+
+    "properly render boolean expressions containing equality expresssions " in {
+      val expr: BooleanExpr = QueryParser.parse("ab.cd = 12").get
+      compressWS(renderer.render(expr)) === "ab.cd = ( 12 )"
+    }
+
+    "properly render comparison expressions contain boolean literal" in {
+      val expr = QueryParser.parse("ab.cd = TRUE").get
+      compressWS(renderer.render(expr)) === "ab.cd = ( true )"
+    }
+
+    "properly render simple equality expression " in {
+      val expr = QueryParser.parse("properties.foo = 'bar1'").get
+      compressWS(renderer.render(expr)) === "foo = ( 'bar1' )"
+    }
+
+    "properly render boolean expressions containing comparison expresssions other than equality " in {
+
+      val expr1 = QueryParser.parse("ab.cd > 12").get
+      val expr2 = QueryParser.parse("ab.cd >= 12").get
+      val expr3 = QueryParser.parse("ab.cd != 'abc'").get
+
+      (
+        compressWS(renderer.render(expr1)) === "ab.cd > ( 12 )"
+      ) and (
+          compressWS(renderer.render(expr2)) === "ab.cd >= ( 12 )"
+        ) and (
+            compressWS(renderer.render(expr3)) === "ab.cd != ( 'abc' )"
+          )
+    }
+
+    "properly render negated expressions " in {
+      val expr1 = QueryParser.parse("not ab.cd = 12").get
+      compressWS(renderer.render(expr1)) === "NOT ( ab.cd = ( 12 ) )"
+    }
+
+    "properly render AND expressions " in {
+      val expr1 = QueryParser.parse(" (ab = 12) and (cd > 'c') ").get
+      compressWS(renderer.render(expr1)) ===
+        "( ab = ( 12 ) ) AND ( cd > ( 'c' ) )"
+    }
+
+    "properly render OR expressions " in {
+      val expr1 = QueryParser.parse(" (ab = 12) or (cd > 'c') ").get
+      compressWS(renderer.render(expr1)) ===
+        "( ab = ( 12 ) ) OR ( cd > ( 'c' ) )"
+    }
+
+    "properly render a boolean literal as a boolean expression" in {
+      val expr = QueryParser.parse("TRUE").get
+      renderer.render(expr) === " true "
+    }
+
+    "property render an IN predicate expression" in {
+      val expr1 = QueryParser.parse(" a.b in (1,2,3) ").get
+      compressWS(renderer.render(expr1)) ===
+        "a.b in (3,2,1)"
+    }
+
+    "properly render simple regex expression " in {
+      val expr = QueryParser.parse("properties.foo ~ /bar1.*/").get
+      compressWS(renderer.render(expr)) === "foo ~ 'bar1.*'"
+    }
+
+    "properly render simple like expression " in {
+      val expr = QueryParser.parse("properties.foo like 'a%bcd'").get
+      compressWS(renderer.render(expr)) === "foo ilike 'a%bcd'"
+    }
+
+    "properly render IS NULL expression " in {
+      val expr = QueryParser.parse("properties.foo is null").get
+      compressWS(renderer.render(expr)) === "foo is null"
+    }
+
+    "properly render IS NOT NULL expression " in {
+      val expr = QueryParser.parse("properties.foo is not null").get
+      compressWS(renderer.render(expr)) === "foo is not null"
+    }
+
+  }
+
   private def compressWS(str: String) = str.replaceAll(" +", " ").trim
 
 }
